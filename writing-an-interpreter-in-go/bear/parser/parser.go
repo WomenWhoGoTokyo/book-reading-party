abc7ff34cb7eb0c435edf7914cf9ab945cbf1243
mi-bear <kumajet@gmail.com>
2021-01-31 12:04:09 +0900

    add party-08 (writing-an-interpreter-in-go)

--- writing-an-interpreter-in-go/bear/parser/parser.go
+++ writing-an-interpreter-in-go/bear/parser/parser.go
@@ -54,6 +54,9 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
+
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -234,6 +237,13 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	*/
 
 	return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{
+		Token: p.curToken,
+		Value: p.curTokenIs(token.TRUE),
+	}
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
--- writing-an-interpreter-in-go/party-08/README.md
+++ writing-an-interpreter-in-go/party-08/README.md
@@ -2,8 +2,15 @@
 ## 読んだところ
 - 2章
   - 2.8 構文解析器の拡張
+    - 2.8.1 真偽値リテラル
 
 ## Note
 - P85 2.8 構文解析器の拡張 から
+- parser_test.go を持ってきて、testInfixExpression と testInfixExpression を利用できるようにしなければならない。(あとでやることにした)
+- P88 の TestBooleanExpression は本にはなくて、parser_test.go の中にある。
+- 2.8.1 真偽値リテラルのテストを追加したときに testIntegerLiteral の直接呼び出し部分を削除した。
 
 ## 次回
+- 2021-02-11 10:00 〜 (仮)
+- P91 2.8.2 グループ化された式 から
+  - 本の parser_test.go を見ておくと幸せかも。
