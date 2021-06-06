6dd2caf1bcacc2615a7db90dc016718a30b2ffeb
mi-bear <kumajet@gmail.com>
2021-06-06 11:29:58 +0900

    add party-16 (writing-an-interpreter-in-go) added chapter-3 code

---
+++ writing-an-interpreter-in-go/ src/chapter-03/bear/repl/repl.go
@@ -0,0 +1,63 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"bear/evaluator"
+	"bear/lexer"
+	"bear/object"
+	"bear/parser"
+)
+
+const PROMPT = "ʕ ꉺᴥ ꉺʔ>> "
+
+const BEAR_FACE = `
+|:::::::::::::::::::::::::::::::
+|"￣￣ﾞﾞヾつ::::::::::::::::::::::
+|,ノ　　ヽ,ヽ:::::::::::::::::::::::::
+| ꄱ　  ꄱ  i'ﾞ￣￣ﾞﾞﾞ'':::::::::::::::
+|　(_o_)　　ﾐノ　　ヽヾつ:::::::::::
+|  ヽノ　　ノ ꉺ　  ꉺ  i::::::::::
+{ヽ,__　　 )   (_o_)　',ﾐ::::::::
+| ヽ　　　/    ヽノ ,ノ::::🍯🍯🍯
+`
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, BEAR_FACE)
+	io.WriteString(out, "Woops! Web ran into some bear business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
---
+++ writing-an-interpreter-in-go/ src/chapter-03/bear/token/token.go
@@ -0,0 +1,66 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// 演算子
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
