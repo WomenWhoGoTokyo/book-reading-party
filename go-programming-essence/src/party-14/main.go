04ce50b66b215a7a3f6ed760e4f1a692960c8aa4
mom0tomo <momo76w@gmail.com>
2023-12-03 11:39:33 +0900

    10.3 TODOの抽出

--- go-programming-essence/src/party-14/main.go
+++ go-programming-essence/src/party-14/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"net/mail"
+	"net/smtp"
 	"os"
 	"time"
 
@@ -26,6 +31,8 @@ type Todo struct {
 
 func main() {
 	sqldb, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// 接続情報の例
+	// sqldb, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,4 +40,52 @@ func main() {
 
 	db := bun.NewDB(sqldb, pgdialect.New())
 	defer db.Close()
+
+	// TODO 抽出
+	var todos []Todo
+	ctx := context.Background()
+	err = db.NewSelect().Model(&todos).Order("created_at").Where("until is not null").Where("done is false").Scan(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(todos) == 0 {
+		return
+	}
+
+	from := mail.Address{Name: "TODO Reminder", Address: os.Getenv("MAIL_FROM")}
+	var buf bytes.Buffer
+	buf.WriteString("From: " + from.String() + "\r\n")
+	buf.WriteString("To: " + os.Getenv("MAIL_TO") + "\r\n")
+	buf.WriteString("Subject: TODO Reminder\r\n")
+	buf.WriteString("\r\n")
+	buf.WriteString("This is your todo list\n\n")
+	for _, todo := range todos {
+		fmt.Fprintf(&buf, "%s %s\n", todo.Until, todo.Content)
+	}
+
+	smtpAuth := smtp.PlainAuth(
+		os.Getenv("MAIL_DOMAIN"),
+		os.Getenv("MAIL_USER"),
+		os.Getenv("MAIL_PASSWORD"),
+		os.Getenv("MAIL_AUTHSERVER"),
+		// 接続情報の例
+		// "example.com",
+		// "user",
+		// "password",
+		// "auth.example.com",
+	)
+
+	err = smtp.SendMail(
+		os.Getenv("MAIL_SERVER"),
+		// 接続情報の例
+		// "mail.example.com:10025",
+		smtpAuth,
+		from.Address,
+		[]string{os.Getenv("MAIL_TO")},
+		// 接続情報の例
+		// []string{"todo-to@example.com"},
+		buf.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
