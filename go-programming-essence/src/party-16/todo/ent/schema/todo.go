d885c8dc0c0ba8202be8123ae0e5fb72e3fc0761
mom0tomo <momo76w@gmail.com>
2023-12-17 10:50:03 +0900

    ent

---
+++ go-programming-essence/src/party-16/todo/ent/schema/todo.go
@@ -0,0 +1,37 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.Text("text").
+			NotEmpty(),
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable(),
+		field.Enum("status").
+			NamedValues(
+				"InProgress", "IN_PROGRESS",
+				"Completed", "COMPLETED",
+			).
+			Default("IN_PROGRESS"),
+		field.Int("priority").
+			Default(0),
+	}
+}
+
+// Edges of the Todo.
+func (Todo) Edges() []ent.Edge {
+	return nil
+}
---
+++ go-programming-essence/src/party-16/todo/go.mod
@@ -0,0 +1,5 @@
+module todo
+
+go 1.21.4
+
+require entgo.io/ent v0.12.5 // indirect
