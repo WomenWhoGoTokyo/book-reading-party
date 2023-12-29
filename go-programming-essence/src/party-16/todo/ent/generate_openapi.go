135f415a9292afd81d00f5498454a897da0a1de2
mom0tomo <momo76w@gmail.com>
2023-12-29 16:34:43 +0900

    add party-16 (#25)
    * ent
    
    * ent
    
    * ent
    
    * ent
    
    * add party-16

---
+++ go-programming-essence/src/party-16/todo/ent/generate_openapi.go
@@ -0,0 +1,23 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/contrib/entoas"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+	ex, err := entoas.NewExtension()
+	if err != nil {
+		log.Fatalf("creating entoas extension: %v", err)
+	}
+	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ex))
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
