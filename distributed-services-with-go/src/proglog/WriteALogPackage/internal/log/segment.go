b01cc55d7b58ac0277ecda6d6689505fc8624f27
mi-bear <kumajet@gmail.com>
2024-01-21 12:15:36 +0900

    add segment implementation and test

--- distributed-services-with-go/src/proglog/WriteALogPackage/internal/log/segment.go
+++ distributed-services-with-go/src/proglog/WriteALogPackage/internal/log/segment.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"path/filepath"
 
-	// api "github.com/WomenWhoGoTokyo/proglog/api/v1"
-	// "google.golang.org/protobuf/proto"
+	api "github.com/WomenWhoGoTokyo/ploglog/api/v1"
+	"google.golang.org/protobuf/proto"
 )
 
 type segment struct {
@@ -56,7 +56,6 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 	return s, nil
 }
 
-/*
 func (s *segment) Append(record *api.Record) (offset uint64, err error) {
 	cur := s.nextOffset
 	record.Offset = cur
@@ -79,4 +78,46 @@ func (s *segment) Append(record *api.Record) (offset uint64, err error) {
 	s.nextOffset++
 	return cur, nil
 }
-*/
+
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
+
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes ||
+		s.index.size >= s.config.Segment.MaxIndexBytes ||
+		s.index.isMaxed()
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
