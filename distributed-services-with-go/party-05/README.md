7a715d820cc7ae6312088924da2039b0e1711785
mi-bear <kumajet@gmail.com>
2024-02-04 12:10:49 +0900

    add party-05

---
+++ distributed-services-with-go/party-05/README.md
@@ -0,0 +1,25 @@
+# Party-04 2024-02-04
+正誤表: https://yoshikishibata.github.io/myhomepage/errata/distributed_services_in_go.html  
+コード: https://github.com/YoshikiShibata/proglog
+
+## 参加者
+- micchie
+- mom0tomo
+- marie
+
+## 読んだところ
+- 3章 ログパッケージの作成
+  - 3.3.4 ログのコーディング
+- 4章 gRPCによるリクエスト処理
+  - 4.1 gRPCとはなにか
+  - 4.2 サービス構築の目標
+  - 4.3 gRPCサービスの定義
+  - 4.4 gRPCプラグインでコンパイル
+  - 4.5 gRPCサーバの実装
+
+## Note
+[Discord Thread](https://discord.com/channels/689414179752247409/1203506337099546685)
+
+## 次回
+- 2024-02-18 (日) 10:00
+- 4章 4.5.1 gRPCでのエラー処理
--- distributed-services-with-go/src/proglog/WriteALogPackage/internal/log/log.go
+++ distributed-services-with-go/src/proglog/WriteALogPackage/internal/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"sort"
@@ -132,4 +133,76 @@ func (l *Log) Reset() error {
 		return err
 	}
 	return l.setup()
+}
+
+func (l *Log) LowestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	return l.segments[0].baseOffset, nil
+}
+
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	return l.highestOffset()
+}
+
+func (l *Log) highestOffset() (uint64, error) {
+	off := l.segments[len(l.segments)-1].nextOffset
+	if off == 0 {
+		return 0, nil
+	}
+	return off - 1, nil
+}
+
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	var segments []*segment
+	for _, s := range l.segments {
+		if s.nextOffset <= lowest+1 {
+			if err := s.Remove(); err != nil {
+				return err
+			}
+			continue
+		}
+		segments = append(segments, s)
+	}
+	l.segments = segments
+	return nil
+}
+
+func (l *Log) Reader() io.Reader {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	readers := make([]io.Reader, len(l.segments))
+	for i, segment := range l.segments {
+		readers[i] = &originReader{segment.store, 0}
+	}
+	return io.MultiReader(readers...)
+}
+
+type originReader struct {
+	*store
+	off int64
+}
+
+func (o *originReader) Read(p []byte) (int, error) {
+	n, err := o.ReadAt(p, o.off)
+	o.off += int64(n)
+	return n, err
+}
+
+func (l *Log) newSegment(off uint64) error {
+	s, err := newSegment(l.Dir, off, l.Config)
+	if err != nil {
+		return err
+	}
+	l.segments = append(l.segments, s)
+	l.activeSegment = s
+	return nil
 }
