0cdb704455d2ad6faa487890a60ef6af56bf496f
mi-bear <kumajet@gmail.com>
2024-03-10 11:52:22 +0900

    added party-08

--- distributed-services-with-go/src/proglog/SecureYourServices/go.mod
+++ distributed-services-with-go/src/proglog/SecureYourServices/go.mod
@@ -3,6 +3,7 @@ module github.com/WomenWhoGoTokyo/ploglog
 go 1.21.6
 
 require (
+	github.com/casbin/casbin v1.9.1
 	github.com/stretchr/testify v1.8.4
 	github.com/tysonmote/gommap v0.0.2
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17
@@ -11,6 +12,7 @@ require (
 )
 
 require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- distributed-services-with-go/src/proglog/SecureYourServices/internal/server/server_test.go
+++ distributed-services-with-go/src/proglog/SecureYourServices/internal/server/server_test.go
@@ -45,7 +45,9 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	require.NoError(t, err)
 
 	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-		CAFile: config.CAFile,
+		CertFile: config.ClientCertFile,
+		KeyFile:  config.ClientKeyFile,
+		CAFile:   config.CAFile,
 	})
 	require.NoError(t, err)
 
@@ -63,6 +65,7 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		KeyFile:       config.ServerKeyFile,
 		CAFile:        config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server:        true,
 	})
 	require.NoError(t, err)
 	serverCreds := credentials.NewTLS(serverTLSConfig)
